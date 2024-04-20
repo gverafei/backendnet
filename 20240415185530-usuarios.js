c8443eff5702a2a17b52735227ed1463301189b5
gverafei <143739705+gverafei@users.noreply.github.com>
2024-04-20 00:45:42 -0600

    Update 20240415185530-usuarios.js

--- 20240415185530-usuarios.js
+++ 20240415185530-usuarios.js
@@ -1,12 +1,13 @@
 'use strict';
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
+const crypto = require('crypto')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
         await queryInterface.bulkInsert('usuario', [
-            { id: 1, username: 'gvera@uv.mx', passwordhash: await bcrypt.hash('patito', 10), nombre: 'Guillermo Vera', rol: 'Administrador', createdAt: new Date(), updatedAt: new Date() },
-            { id: 2, username: 'patito@uv.mx', passwordhash: await bcrypt.hash('patito', 10), nombre: 'Usuario patito', rol: 'Usuario', createdAt: new Date(), updatedAt: new Date() }
+            { id: crypto.randomUUID(), username: 'gvera@uv.mx', passwordhash: await bcrypt.hash('patito', 10), nombre: 'Guillermo Vera', rol: 'Administrador', createdAt: new Date(), updatedAt: new Date() },
+            { id: crypto.randomUUID(), username: 'patito@uv.mx', passwordhash: await bcrypt.hash('patito', 10), nombre: 'Usuario patito', rol: 'Usuario', createdAt: new Date(), updatedAt: new Date() }
         ]);
     },
 
